ea9697dbcb7b0b809adbd650d94df2fa4ebd275d
lavr174 <lavr174@icloud.com>
2022-08-04 18:50:49 +0300

    if_else

---
+++ If_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ведите имя пользовател ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
